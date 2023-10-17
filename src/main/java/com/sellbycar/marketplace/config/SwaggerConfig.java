00fb60a3db92d797a289221921c6fd6f1324e80a
arturhasparian <a.hasparian@gmail.com>
2023-10-18 00:37:31 +0300

    Marketplace v1.1 Done

--- src/main/java/com/sellbycar/marketplace/config/SwaggerConfig.java
+++ src/main/java/com/sellbycar/marketplace/config/SwaggerConfig.java
@@ -19,8 +19,8 @@ public class SwaggerConfig {
                         .description("REST API for user database")
                         .termsOfService("s"))
                 .addServersItem(new Server()
-//                        .url("https://51.20.169.250:8443/")
-                        .url("https://localhost:8443/")
+                        .url("https://16.171.118.114:8443/")
+//                        .url("https://localhost:8443/")
                         .description("Future Server for Marketplace"));
     }
 }
