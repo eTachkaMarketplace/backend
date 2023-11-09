8cb4fd3068c171a4a13ed4d261c27f9220c15555
arturhasparian <a.hasparian@gmail.com>
2023-11-10 01:14:52 +0200

    Fix Typo

--- src/main/java/com/sellbycar/marketplace/repository/AdvertisementRepository.java
+++ src/main/java/com/sellbycar/marketplace/repository/AdvertisementRepository.java
@@ -3,5 +3,5 @@ package com.sellbycar.marketplace.repository;
 import com.sellbycar.marketplace.repository.model.Advertisement;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface AdvestisementRepository extends JpaRepository<Advertisement,Long> {
+public interface AdvertisementRepository extends JpaRepository<Advertisement,Long> {
 }
