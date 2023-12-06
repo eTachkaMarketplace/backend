98e91402e12ac6bd98a253538541f643356f81f9
arturhasparian <a.hasparian@gmail.com>
2023-12-06 14:23:43 +0200

    Rollback

--- src/main/resources/db/migration/V0001__createNewTable_favCars.sql
+++ src/main/resources/db/migration/V0001__createNewTable_favCars.sql
@@ -1,8 +1,8 @@
--- CREATE TABLE users_advertisements
--- (
---     user_id BIGINT NOT NULL
---         REFERENCES users (id),
---     advertisements_id INT    NOT NULL
---         REFERENCES cars (id),
---     PRIMARY KEY (user_id, advertisements_id)
--- );
+CREATE TABLE users_advertisements
+(
+    user_id BIGINT NOT NULL
+        REFERENCES users (id),
+    advertisements_id INT    NOT NULL
+        REFERENCES cars (id),
+    PRIMARY KEY (user_id, advertisements_id)
+);
