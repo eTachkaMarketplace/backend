a0be3fc52ab7af03390e8a4de322600cb501a813
arturhasparian <a.hasparian@gmail.com>
2023-12-29 13:59:59 +0200

    Fix hard code. Change SwaggerConfig

--- src/main/java/com/sellbycar/marketplace/configs/SwaggerConfig.java
+++ src/main/java/com/sellbycar/marketplace/configs/SwaggerConfig.java
@@ -3,11 +3,15 @@ package com.sellbycar.marketplace.configs;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
+
+    @Value("${SERVER_SOURCE}")
+    private String serverURL;
 
     @Bean
     OpenAPI customOpenApi() {
@@ -18,7 +22,7 @@ public class SwaggerConfig {
                         .description("REST API for user database")
                         .termsOfService("s"))
                 .addServersItem(new Server()
-                        .url("https://185.69.153.118:8443/")
+                        .url(serverURL)
                         .description("Future Server for Marketplace"));
     }
 }
