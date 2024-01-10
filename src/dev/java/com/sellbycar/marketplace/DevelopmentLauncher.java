e8f8d9d6296e7b8e85dc02207a08983f2193d54b
Shorinami <shorinami@atombuilt.com>
2024-01-10 22:33:54 +0200

    fix: docker check does not work properly on windows
    [ci skip]

--- src/dev/java/com/sellbycar/marketplace/DevelopmentLauncher.java
+++ src/dev/java/com/sellbycar/marketplace/DevelopmentLauncher.java
@@ -1,20 +1,18 @@
 package com.sellbycar.marketplace;
 
 import lombok.extern.slf4j.Slf4j;
-
-import java.io.File;
+import org.testcontainers.DockerClientFactory;
 
 @Slf4j
 public class DevelopmentLauncher {
 
     public static void main(String[] args) {
-        checkDocker();
+        exitIfDockerNotRunning();
         MarketplaceApplication.main(args);
     }
 
-    private static void checkDocker() {
-        File dockerSocket = new File("/var/run/docker.sock");
-        if (!dockerSocket.exists()) {
+    private static void exitIfDockerNotRunning() {
+        if (!DockerClientFactory.instance().isDockerAvailable()) {
             log.error("Install and run Docker to boot the development environment.");
             Runtime.getRuntime().exit(1);
         }
