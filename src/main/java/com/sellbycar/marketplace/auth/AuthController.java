8374caf302cedca60f04f685ab5854f5bf29216d
arturhasparian <a.hasparian@gmail.com>
2024-01-15 18:53:46 +0200

    refactoring: Remove Security Requirement from methods getNewAcessToken and getNewRefreshToken

--- src/main/java/com/sellbycar/marketplace/auth/AuthController.java
+++ src/main/java/com/sellbycar/marketplace/auth/AuthController.java
@@ -7,7 +7,6 @@ import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
-import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.mail.MessagingException;
 import jakarta.security.auth.message.AuthException;
